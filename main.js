b39fb90eaeb3d0b718727ec62c7eba7a0d089681
Aldana Abril Longhi <aldulonghi@gmail.com>
2020-11-05 12:31:31 -0300

    Build displayBooks function

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const book1 = new Book('La tregua', 'Mario Benedetti', '300', true);
+const book2 = new Book('Harry Potter', 'JK Rowling', '1000', true);
 let myLibrary = [book1, book2];
 
 function Book(title, author, pagesNum, read) {
@@ -11,19 +13,23 @@ function addBookToLibrary() {
   
 }
 
-const bookCard = document.querySelector('#books-table')
 
 function displayBooks(books) {
+  var bookCard = document.querySelector('#books-table')
   for (let book in books) {
     let tableRow = document.createElement('tr');
-    let bookTitle = document.createElement('td');
-    let bookTitle = document.createTextNode(books[book].title);
-    tableRow.appendChild(bookTitle);
+    let bookKeys = Object.values(books[book]);
+    console.log(bookKeys);
+    for (let key in bookKeys) {
+      let bookTitle = document.createElement('td');
+      let bookTitleContent = document.createTextNode(bookKeys[key]);
+      bookTitle.appendChild(bookTitleContent);
+      tableRow.appendChild(bookTitle);
+    } 
     bookCard.appendChild(tableRow);
   }
 }
 
-const book1 = new Book('La tregua', 'Mario Benedetti', '300', true);
-const book2 = new Book('Harry Potter', 'JK Rowling', '1000', true);
 
 displayBooks(myLibrary);
+
